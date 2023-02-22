a0b427bc2f3d92634bf0bc954a3c2b06c900afab
Sebastian Gonzalez <sebastian.gonzalez@kyndryl.com>
2023-02-22 10:52:10 -0300

    Updates

--- tkinter/main.py
+++ tkinter/main.py
@@ -5,7 +5,7 @@ window.title("TK")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text = "This is a label", font=("Arial", 30, "bold"))
-my_label.pack()
+my_label.pack(side="left")
 
 
 window.mainloop()
