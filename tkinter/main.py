d8be5772591a9d51f7754288da19053347bf01ea
Sebastian Gonzalez <sebastian.gonzalez@kyndryl.com>
2023-02-22 12:02:15 -0300

    Added tkinter, input and button

--- tkinter/main.py
+++ tkinter/main.py
@@ -7,5 +7,16 @@ window.minsize(width=500, height=300)
 my_label = tkinter.Label(text = "This is a label", font=("Arial", 30, "bold"))
 my_label.pack(side="left")
 
+def button_clicked():
+    # print("I was clicked")
+    # my_label.config(text="Clicked!")
+    my_label.config(text=input.get())
+
+
+button = tkinter.Button(text = "Click me!", command=button_clicked)
+button.pack()
+
+input = tkinter.Entry(width=20)
+input.pack()
 
 window.mainloop()
