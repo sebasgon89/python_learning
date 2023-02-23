2f5b351d3f3baa02d7914a1978a99f94825d30fc
Sebastian Gonzalez <sebastian.gonzalez@kyndryl.com>
2023-02-23 12:29:56 -0300

    Updated Pomodoro with timer

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -14,16 +14,34 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+def count_down(count):
+    print(count)
+    if count > 0:
+        window.after(1000, count_down(count - 1))
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+count_down(5)
+
+title_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 canvas = tkinter.Canvas(width=200, height=224, bg = YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text = "00.00", fill = "white", font = (FONT_NAME, 36, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = tkinter.Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+restart_button = tkinter.Button(text="Restart", highlightthickness=0)
+restart_button.grid(column=2, row=2)
+
+check_marks = tkinter.Label(text="✓", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
