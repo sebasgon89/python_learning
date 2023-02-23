4ca0421842baea58e31b68b206a8bea2c58eca39
Sebastian Gonzalez <sebastian.gonzalez@kyndryl.com>
2023-02-23 17:37:15 -0300

    Updated Pomodoro with timer

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -12,19 +12,19 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    count_down(5)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    print(count)
+    canvas.itemconfig(timer_text, text=count)
     if count > 0:
-        window.after(1000, count_down(count - 1))
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 
-count_down(5)
 
 title_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
@@ -32,10 +32,10 @@ title_label.grid(column=1, row=0)
 canvas = tkinter.Canvas(width=200, height=224, bg = YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text = "00.00", fill = "white", font = (FONT_NAME, 36, "bold"))
+timer_text = canvas.create_text(100, 130, text = "00.00", fill = "white", font = (FONT_NAME, 36, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = tkinter.Button(text="Start", highlightthickness=0)
+start_button = tkinter.Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 restart_button = tkinter.Button(text="Restart", highlightthickness=0)
